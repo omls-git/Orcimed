3b93b6d63662a8557226908246ae7ae177e96e46
Jyotigahali <jyoti.ghali@orcimedlifesciences.com>
2025-02-28 01:04:12 +0530

    get and post api for career

--- Omls.Functions/index.js
+++ Omls.Functions/index.js
@@ -1,17 +1,33 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const db = require('./models');
+const cors = require('cors');
+//const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(express.json());
+app.use(cors());
 
-// Sample Route
-app.get('/', (req, res) => {
-  res.send('Hello, Welcome to OMLS Website!!!!!!!!!');
-});
+const PostRouter = require('./routes/Posts');
+app.use("/posts", PostRouter);
 
-// Start Server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+db.sequelize.sync().then(() => {  
+  app.listen(5000, () => {  
+    console.log("server running in server port 5000");
+   });
+});   
+
+
+
+// // Middleware
+// app.use(express.json());
+
+// // Sample Route
+// app.get('/', (req, res) => {
+//   res.send('Hello, Welcome to OMLS Website!!!!!!!!!');
+// });
+
+// // Start Server
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
