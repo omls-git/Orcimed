23e443e6ff6cd0cdd1b70cb56138844d82ff63a7
rohithDudam <rohith.dudam@intrinsics.com>
2025-03-11 14:27:36 +0530

    header responsive

--- Omls.Client/src/pages/ContactScreen.js
+++ Omls.Client/src/pages/ContactScreen.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import Header from '../components/Header'
 
 const ContactScreen = () => {
   return (
-    <div><Header /></div>
+    <div>ContactScreen</div>
   )
 }
 
--- Omls.Client/src/pages/ServicesScreen.js
+++ Omls.Client/src/pages/ServicesScreen.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import Header from '../components/Header'
 
 const ServicesScreen = () => {
   return (
-    <div><Header /></div>
+    <div>Service Screen</div>
   )
 }
 
