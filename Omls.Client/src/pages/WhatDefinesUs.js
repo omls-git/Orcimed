fb0297eaa6f127ae261eb80bfea81ca273e496a0
rohithDudam <rohith.dudam@intrinsics.com>
2025-04-02 20:31:10 +0530

    homescreen changes

---
+++ Omls.Client/src/pages/WhatDefinesUs.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { Box, Container, useScrollTrigger} from "@mui/material";
+import { motion } from "framer-motion";
+import definesUs from '../images/definesusimg.png'
+
+const WhatDefinesUs = () => {
+  const [isInView, setIsInView] = useState(false);
+  const trigger = useScrollTrigger({ disableHysteresis: true, threshold: 200 });
+
+  useEffect(() => {
+    if (trigger) setIsInView(true);
+  }, [trigger]);
+
+  return (
+    <Box sx={{ backgroundColor: "black", pt: 6, textAlign: "center", }}>
+      <Container sx={{ position: "relative", overflow: "hidden" }}>
+        <motion.img
+          src={definesUs}
+          alt="What Defines Us"
+          initial={{ y: 500, opacity: 0 }}
+          animate={isInView ? { y: 0, opacity: 1 } : { y: 500, opacity: 0 }}
+          transition={{ duration: 3, ease: "easeOut" }}
+          style={{ width: "50%", maxWidth: "250px" }}
+        />
+      </Container>
+    </Box>
+  );
+};
+
+export default WhatDefinesUs;
