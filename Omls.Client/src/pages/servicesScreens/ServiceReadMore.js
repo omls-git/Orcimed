6f4d3be6fe69e246d82876c74459d3c77aea05bc
omls-git <it@orcimedlifesciences.com>
2025-04-22 12:40:52 +0530

    Update ServiceReadMore.js

--- Omls.Client/src/pages/servicesScreens/ServiceReadMore.js
+++ Omls.Client/src/pages/servicesScreens/ServiceReadMore.js
@@ -3,7 +3,7 @@ import { Box, Typography, Grid, useTheme, useMediaQuery } from "@mui/material";
 import { motion } from "framer-motion";
 import services from './ServicesFile.json'
 import Pharmacovigilance from "./Pharmacovigilance";
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const containerVariants = {
   hidden: {},
