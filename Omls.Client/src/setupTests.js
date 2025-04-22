27fbb9e14b290d8281432ff4ee793d6ca404a21f
omls-git <it@orcimedlifesciences.com>
2025-04-22 13:17:35 +0530

    test changes

--- Omls.Client/src/setupTests.js
+++ Omls.Client/src/setupTests.js
@@ -1,5 +1,15 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+// // jest-dom adds custom jest matchers for asserting on DOM nodes.
+// // allows you to do things like:
+// // expect(element).toHaveTextContent(/react/i)
+// // learn more: https://github.com/testing-library/jest-dom
+// import '@testing-library/jest-dom';
+import '@testing-library/jest-dom';  // Your existing imports
+import matchMediaMock from 'match-media-mock';
+
+// Mock matchMedia for jest
+matchMediaMock.useMediaQuery = jest.fn().mockImplementation((query) => ({
+  matches: query.includes('max-width') ? true : false,
+  media: query,
+  addListener: jest.fn(),
+  removeListener: jest.fn(),
+}));
