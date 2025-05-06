44d3257187350d82f7378c151f4175729c2b5e88
omls-git <it@orcimedlifesciences.com>
2025-05-06 13:23:48 +0530

    Update HeroSection.js

--- Omls.Client/src/pages/HeroSection.js
+++ Omls.Client/src/pages/HeroSection.js
@@ -9,7 +9,7 @@ const HeroSection = () => {
   const isTab = useMediaQuery('(min-width: 425px) and (max-width: 768px)');
 
   const heroContent1 = "Built by industry leaders with decades of experiences in clinical research and marketed product support, OMLS is here to provide you the services you need to be successful.  OMLS is fast, high quality and high value.  From clinical trials to post-market support, OMLS offers end-to-end solutions tailored to your needs. With an unwavering commitment to quality, transparency, and value, we are your trusted offshore partner."
-// const heroContent2 = 'We are a quality driven Clinical Services Organisation (CSO) focused on delivering accelerated innovative solutions and reliable scientific services.'
+
 
 const navigate = useNavigate();
 
@@ -44,9 +44,6 @@ const navigate = useNavigate();
             <Typography variant="h6" mt={2} color="rgba(14, 11, 11, 0.86)" 
             width={isMobile ? "100%": isTab ? "70%" : "40%"}  fontFamily={'inherit'} //fontWeight={'bold'}
             >{heroContent1}</Typography>
-            // <Typography variant="h6" mt={2} color="rgba(14, 11, 11, 0.86)" 
-            // width={isMobile ? "100%": isTab ? "70%" : "40%"}  fontFamily={'inherit'} //fontWeight={'bold'}
-            // >{heroContent2}</Typography>
             <Button
               variant="contained"
               color="warning"
